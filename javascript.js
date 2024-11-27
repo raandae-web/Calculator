9f9fff2e109b472751d793ac15d2b1eb10feaa35
raandae-web <chidoziee123@gmail.com>
2024-11-26 18:36:33 -0600

    Calculator done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,100 @@
-console.log("Hello Wrld!")
+let add = function(a, b){
+    return a + b;
+}
+
+let subtract = function(a,b){
+    return a - b;
+}
+
+let multiply = function(a,b){
+    return a * b;
+}
+
+let divide = function(a,b){
+    return a / b;
+}
+
+let value1 = 0;
+
+let operator = "";
+
+let value2 = 0;
+
+let numberVal = "";
+
+let numberVal2 = ""
+
+
+
+
+    
+const numbersContainer = document.querySelector(".numbers-container");
+
+const totalDisplay = document.querySelector(".total-display")
+
+const operatorContainer = document.querySelector(".operator-container")  
+
+
+numbersContainer.addEventListener("click", (event) => {
+   
+    if (operator === "") {
+        
+        numberVal += event.target.textContent; 
+        value1 = parseInt(numberVal);
+        totalDisplay.textContent = value1
+    } else {
+        
+        numberVal2 += event.target.textContent;
+        value2 = parseInt(numberVal2);
+        totalDisplay.textContent = value2
+    }
+});
+
+
+
+
+operatorContainer.addEventListener("click", (event)=>{
+    const clickedOperator = event.target.textContent;
+
+    if (clickedOperator !== "=" && clickedOperator !== '') {
+       
+        operator = clickedOperator;
+        totalDisplay.textContent = operator
+    }
+
+    if (clickedOperator === "=") {
+        
+        operate(value1, operator, value2);
+
+        value1 = 0;
+        value2 = 0;
+        operator = "";
+    }
+});
+
+
+
+let operate = function(v1, op, v2){
+
+    let result;
+    switch(op) {
+    case '+':
+        result = add(v1, v2);
+        break;
+    case '-':
+        result = subtract(v1, v2);
+        break;
+    case '*':
+        result = multiply(v1, v2);
+        break;
+    case '/':
+        result = divide(v1, v2);
+        break;
+    default:
+        result = "Error"; 
+        break;
+
+}
+totalDisplay.textContent = result;  
+}
+
